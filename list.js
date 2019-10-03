a1d0e71e9d2353e6c8ebf37cf7be29fe7ca535a1
Nicolas Van Oost <nico.vanoost@gmail.com>
2019-10-03 18:45:31 -0300

    casi terminado el maquetado

---
+++ list.js
@@ -0,0 +1,38 @@
+$( document ).ready(function(){
+    $( "#2" ).sortable({
+        axis: "x",
+        containment: "parent",
+        cursor: "move"
+    });
+});
+    
+
+
+
+
+function editItem(){
+
+};
+
+function checkLength(){
+    
+}
+
+function deleteItem(){
+
+};
+
+
+function updateCounter(){
+
+};
+
+
+function saveList(){
+
+};
+
+
+function retrieveList(){
+
+};
