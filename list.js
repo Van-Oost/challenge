bd250f415d0c8232d61afd77db46c58026334036
Nicolas Van Oost <nico.vanoost@gmail.com>
2019-10-04 01:49:44 -0300

    retrieve

--- list.js
+++ list.js
@@ -1,4 +1,5 @@
 $( document ).ready(function(){
+    retrieveList()
     updateCounter()
     dragNdrop()
 });
@@ -7,12 +8,11 @@ $( document ).ready(function(){
 function dragNdrop(){
     $( "#sortable" ).sortable({
         items:".sortable-item",
-        
         containment: "parent",
         cursor: "move",
         animation: 150,
-
     });
+    saveList()
 }
 
 
@@ -31,18 +31,44 @@ function deleteItem(){
 
 function updateCounter(){
     var itemCount = document.getElementById("sortable").getElementsByClassName("sortable-item").length;
-    var counter= document.getElementById("counter");
+    var counter = document.getElementById("counter");
     counter.innerHTML = itemCount+" ITEMS";
 };
 
 
 function saveList(){
+        var listContents = [];
+        $("#sortable").each(function(){
+           listContents.push(this.innerHTML);
+        })
+        localStorage.setItem("itemsList", JSON.stringify(listContents));
+        console.log(JSON.stringify(listContents))
 
 };
 
 
 function retrieveList(){
-
+    if (localStorage.getItem("itemsList") !== null){
+    var obtainedList = JSON.parse(localStorage.getItem("itemsList"));
+    $( "#sortable" ).innerHTML = obtainedList;
+    }
 };
+
+/* 
+<script>
+$(function(){
+    var order=JSON.parse(localStorage.order||"[]")
+    $.each(order,function(i,id){
+        $("#"+id).appendTo($( '#sortable' ))
+    })
+    $( '#sortable' ).sortable({
+        stop:stop,
+    });
+    function stop(e,ui){
+        var order=$(this).children().map(function(){return this.id}).get()
+        localStorage.order=JSON.stringify(order)
+    }
+})
+</script> */
 
 
